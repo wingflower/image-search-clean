69ee0c0ec48b7d8b7ab5017110f3939c85492854
Dongho Gwak <wing.flower.k@gmail.com>
2024-06-29 16:05:11 +0900

    feat: Clean Architecture

---
+++ lib/clean/clean_main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:image_search_clean/clean/ui/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Search Clean App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(centerTitle: true),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
