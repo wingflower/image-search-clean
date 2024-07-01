d433538d43045f690da6ca771d7755410a9b9960
Dongho Gwak <wing.flower.k@gmail.com>
2024-07-01 14:29:20 +0900

    refactor: Clean전, 정적화면을 위한 Stream 적용

--- lib/clean/data/photo_provider.dart
+++ lib/clean/data/photo_provider.dart
@@ -1,10 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
 import 'package:image_search_clean/clean/data/api.dart';
+import 'package:image_search_clean/clean/model/photo.dart';
 
 class PhotoProvider extends InheritedWidget {
   final PixabayApi api;
 
-  const PhotoProvider({
+  final _photoStreamController = StreamController<List<Photo>>()..add([]);
+  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
+
+  PhotoProvider({
     super.key,
     required this.api,
     required super.child,
@@ -15,6 +22,11 @@ class PhotoProvider extends InheritedWidget {
         context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
     assert(result != null, 'No');
     return result!;
+  }
+
+  Future<void> fetch(String query) async {
+    final result = await api.fetch(query);
+    _photoStreamController.add(result);
   }
 
   @override
